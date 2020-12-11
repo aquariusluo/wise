6383eacf6ad6c9382528c71353e9920dd762b407
aquariusluo@gmail.com <aquariusluo@gmail.com>
2020-12-11 18:20:13 +0800

    wise

--- src/mapping.ts
+++ src/mapping.ts
@@ -10,19 +10,19 @@ import {
   UniSwapResult,
   WiseReservation
 } from "../generated/Wise/Wise"
-import { ExampleEntity } from "../generated/schema"
+import { Wise } from "../generated/schema"
 
 export function handleGeneratedRandomSupply(
   event: GeneratedRandomSupply
 ): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = Wise.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new Wise(event.transaction.from.toHex())
 
     // Entity fields can be set using simple assignments
     entity.count = BigInt.fromI32(0)
